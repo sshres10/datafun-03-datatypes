29e915e189fe4380b773bfd12c45b86d574435fc
Sushanta Shrestha <sshres10@gmail.com>
2023-09-10 11:10:16 -0400

    Update

--- user_string_lists.py
+++ user_string_lists.py
@@ -47,5 +47,45 @@ def process_text_soccer():
         words = text.split()  # split on whitespace
         unique_words = sorted(set(words))  # remove duplicates by making a set
         logging.info(f"There are {len(unique_words)} unique words in soccer.txt.")
+
+# -------------------------------------------------------------
+# Call some functions and execute code!
+# Remember, code blocks must be indented consistently after a colon.
+
+
+def process_text_soccer():
+    """Read in text_soccer.txt and process it"""
+    logger.info("Calling process_text_soccer()")
+
+    try:
+        # read in soccer to get a list of words
+        with open("text_soccer.txt", "r") as fileObject:
+            text = fileObject.read()
+            words = text.split()  # split on whitespace
+            unique_words = sorted(set(words))  # remove duplicates by making a set
+            logging.info(f"There are {len(unique_words)} unique words in soccer.txt.")
+    except FileNotFoundError:
+        logging.error("File not found: text_soccer.txt")
+
+def foul():
+    """Which foul is it?"""
+    logger.info("Calling foul()")
+
+    foul_committed = True  # TODO: change this when ready
+    logger.info(f"foul_committed = {foul_committed}")
+
+    if not foul_committed:
+        logger.info("No foul committed yet")
+        logger.info("Existing the function early without further processing.")
+        return
+
+if __name__ == "__main__":
+    logger.info("Calling functions from main block")
+
+    process_text_soccer()
+    show_log()
+  
+        
+    
                               
                               
